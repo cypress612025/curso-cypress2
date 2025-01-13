605371087eb88658d4de7143da11abebd930e03c
cypress <elva.proenza@testgroup.cl>
2025-01-13 14:26:07 -0300

    Creacion de archivos de product Details page

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import { ProductDetailsElements } from "./produvt-details.elements";
+
+export class ProductDetailsMethods{
+static clickOnAddToCartButton(){
+    ProductDetailsElements.buttons.addToCart.click();
+ }
+}
