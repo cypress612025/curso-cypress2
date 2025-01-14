83410636c3e243f2d6723bc75152ee16a0deee26
cypress <elva.proenza@testgroup.cl>
2025-01-14 10:26:21 -0300

    Creacion de pagina CommomPage

---
+++ cypress/e2e/pages/commom-page/commom-page.elements.js
@@ -0,0 +1,24 @@
+export class CommomPageElements{
+    static get topMenu(){
+         return{
+            get home(){
+                return cy.contains('a','Home');
+            },
+            get contac(){
+                return cy.contains('a','Contact');
+            },
+            get aboutUs(){
+                return cy.contains('a','About us');
+            },
+            get cart(){
+                return cy.contains('a','Cart');
+            },
+            get logIn(){
+                return cy.contains('a','Log in');
+            },
+            get signUp(){
+                return cy.contains('a','Sign up');
+            },
+         }
+    }
+}
---
+++ cypress/e2e/pages/commom-page/commom-page.methods.js
@@ -0,0 +1,27 @@
+import { CommomPageData } from "./commom-page.data";
+import { CommomPageElements } from "./commom-page.elements";
+
+export class CommomPageMethods{
+    static navigateToDemoBlaze(){
+        cy.visit(CommomPageData.url);
+    }
+    static clickOnHomeOption(){
+        CommomPageElements.topMenu.home.click();
+    }
+    static clickOnContactOption(){
+        CommomPageElements.topMenu.contac.click();
+    }
+    static clickOnAboutUSOption(){
+        CommomPageElements.topMenu.aboutUs.click();
+    }
+    static clickOnCartOption(){
+        CommomPageMethods.topMenu.cart.click();
+    }
+    static clickOnLogInOption(){
+        CommomPageElements.topMenu.logIn.click();
+    
+    }
+    static clickOnSignUpOption(){
+        CommomPageElements.topMenu.signUp.click();
+    }
+}
